baeee222fa8d04c8cc5a8b6ae6ca2c5245042b58
fellipedoprado <fellipe.doprado@gmail.com>
2025-04-11 14:24:19 -0300

    angular update v18 to v19

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-small-card',
-  templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+    selector: 'app-small-card',
+    templateUrl: './small-card.component.html',
+    styleUrls: ['./small-card.component.css'],
+    standalone: false
 })
 export class SmallCardComponent implements OnInit {
 
